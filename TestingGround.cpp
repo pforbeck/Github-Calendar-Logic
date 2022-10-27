546d95a6b99e67c62fd8001adc3f226837658e54
pforbeck <pat@forbeck.com>
2022-10-26 21:40:40 -0500

    Update TestingGround.cpp

--- TestingGround.cpp
+++ TestingGround.cpp
@@ -4,6 +4,9 @@
 #include <stdexcept>
 #include <string>
 #include <array>
+#include <vector>
+#include <fstream>
+#include <algorithm>
 
 void clearScreen() {
     system("CLS"); // Clear screen
@@ -47,15 +50,100 @@ void menuExecution(int optionNumber) {
         while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
             result += buffer.data();
         }
-        std::cout << result << std::endl;
         clearScreen();
-        if (result.length() > 0) {
-            std::cout << "Success!" << std::endl;
+
+        // Send processed JSON data to "http://192.168.4.1/led" as a curl POST request
+
+        int firstLoc = result.find_first_of("[") + 1; // Trims the front of the data string
+        int lastLoc = result.find_first_of("]"); // Trims the back of the data string
+        std::vector<char> resultsVector; // Puts the string data into a vector
+        for (int i = firstLoc; i < lastLoc; i++) {
+            resultsVector.push_back(result[i]);
+            i++;
         }
 
-        // Execute more code to change the LED array here with the JSON data from "result".
+        std::reverse(resultsVector.begin(), resultsVector.end()); // Reverses the vector so it goes from most to least recent
 
-        return;
+        std::ofstream myfile("LEDData.json"); // Accesses/creates a JSON file that I can output to
+
+        if (!myfile.is_open()) {
+            std::cout << "Error! Exiting program...";
+            exit(1);
+        }
+
+        myfile << "{\n\"count\":" << resultsVector.size() << ",\n\"clear\" : false,\n\"cords\" : [\n";
+
+        std::string coordinateData;
+        int k = 0;
+        int roy, gee, biv;
+        for (int i = 0; i < 32; i++) { // This is for X (NOTE: I am not sure these values are right, please double check)
+            for (int r = 0; r < 8; r++) { // This is for Y
+                coordinateData = "\t[" + std::to_string(i); // Adds the X coord to the string to go into the JSON file
+                coordinateData.append(", " + std::to_string(r)); // Adds the Y coord to the string to go into the JSON file
+                switch (resultsVector.at(k)){
+                case '0': // Why are these chars? I dunno... seriously. I'm losing it.
+                    roy = 0;
+                    gee = 0;
+                    biv = 0;
+                    break;
+                case '1':
+                    roy = 56;
+                    gee = 221;
+                    biv = 52;
+                    break;
+                case '2':
+                    roy = 69;
+                    gee = 160;
+                    biv = 69;
+                    break;
+                case '3':
+                    roy = 4;
+                    gee = 117;
+                    biv = 38;
+                    break;
+                case '4':
+                    roy = 10;
+                    gee = 66;
+                    biv = 8;
+                    break;
+                default:
+                    roy = 10;
+                    gee = 66;
+                    biv = 8;
+                    break;
+                }
+                // Why so many appends? Because it seems to break after one +... so tired
+                coordinateData.append(", " + std::to_string(roy)); // R
+                coordinateData.append(", " + std::to_string(gee)); // G
+                coordinateData.append(", " + std::to_string(biv)); // B
+                coordinateData.append("],"); // Closing bracket
+                k++;
+                if (k == 256) { // Last loop
+                    coordinateData.erase(coordinateData.find_last_of(","));
+                    myfile << coordinateData << std::endl;
+                    coordinateData = "\t]";
+                    myfile << coordinateData << std::endl;
+                    coordinateData = "}";
+                }
+                myfile << coordinateData << std::endl;
+                coordinateData = ""; // Resets the coordinateData string so it can accept more data.
+            }
+        }
+
+
+        std::string ipAdd = "192.168.0.1";
+        //std::string ipAdd = "http://192.168.4.1/led";
+        std::string JSONoutput = "curl -d @LEDData.json " + ipAdd;
+        system(JSONoutput.c_str());
+
+        clearScreen();
+        if (result.length() > 0) {
+            std::cout << "Success!\n" << std::endl;
+        }
+
+        myfile.close(); // Closes file
+
+        return; // Returns to main menu
     }
 
     switch (optionNumber) {
@@ -87,3 +175,18 @@ int main()
     }
     return 0;
 }
+
+/* FOR REFERENCE
+{
+    "count":4,
+        "clear" : false,
+        "cords" : [
+                    X  Y  R   G    B
+                [0, 0, 0, 0, 0],        0
+                [0, 0, 56, 221, 52],    1
+                [0, 1, 0, 160, 0],      2
+                [0, 2, 4, 117, 38],     3
+                [0, 3, 0, 160, 0]       4
+        ]
+}
+*/
